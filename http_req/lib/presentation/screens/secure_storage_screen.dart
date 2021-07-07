bd1a2e2487b5971ef51eb97dea60837b34b4725e
alok-km <manavsuryalok@gmail.com>
2021-07-07 15:28:20 +0800

    secure storage issue resolves

--- http_req/lib/presentation/screens/secure_storage_screen.dart
+++ http_req/lib/presentation/screens/secure_storage_screen.dart
@@ -9,27 +9,29 @@ class SecureStorageScreen extends StatefulWidget {
 }
 
 class _SecureStorageScreenState extends State<SecureStorageScreen> {
+  String? _text1 = '';
+  String? _text2 = '';
+  final SecureStorage secureStorage = SecureStorage();
+
+  Future _save() async {
+    secureStorage.writeSecureData('username', 'alokmanav');
+    secureStorage.writeSecureData('password', '12345678');
+    print("saved");
+  }
+
+  Future _read() async {
+    _text1 = await secureStorage.readSecureData('username');
+    _text2 = await secureStorage.readSecureData('password');
+    print(_text1);
+  }
+
+  Future _delete() async {
+    secureStorage.deleteSecureData('username');
+    secureStorage.deleteSecureData('password');
+  }
+
   @override
   Widget build(BuildContext context) {
-    String _text1 = 'empty';
-    String _text2 = 'empty';
-
-    final SecureStorage secureStorage = SecureStorage();
-
-    Future _save() async {
-      secureStorage.writeSecureData('username', 'alokmanav');
-      secureStorage.writeSecureData('password', '12345678');
-    }
-
-    Future _read() async {
-      _text1 = await secureStorage.readSecureData('username');
-      _text2 = await secureStorage.readSecureData('password');
-    }
-
-    Future _delete() async {
-      secureStorage.deleteSecureData('username');
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Secure Storage'),
@@ -44,8 +46,8 @@ class _SecureStorageScreenState extends State<SecureStorageScreen> {
                 fontSize: 30,
               ),
             ),
-            Text(_text1),
-            Text(_text2),
+            Text(_text1 ?? "<No data>"),
+            Text(_text2 ?? "<No data>"),
             SizedBox(height: 32),
             Divider(),
             SizedBox(height: 32),
